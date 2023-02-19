d53b9eff95529417cca5217e6edd0cb134e35541
abdou.medouni <abdou.medouni@gmail.com>
2023-02-19 16:51:35 +0000

    update

--- initialize.sh
+++ initialize.sh
@@ -4,6 +4,13 @@ initialize_workinfo(){
     #initialize_workinfo
     #echo -e `date +%Y-%m-%d-[%H%M%S]` initialize_workinfo >> /home/runner/runner.log
     #配置工作目录
+    cd /tmp
+    apt install -y wget curl ffmpeg
+    wget https://github.com/hr3lxphr6j/bililive-go/releases/download/v0.7.8/bililive-linux-amd64.tar.gz
+    tar zxvf bililive-linux-amd64.tar.gz
+    chmod +x bililive-linux-amd64
+    mv ./bililive-linux-amd64 /usr/bin/bililive-go
+    rm ./bililive-linux-amd64.tar.gz
     initialize_exit0
 }
 
